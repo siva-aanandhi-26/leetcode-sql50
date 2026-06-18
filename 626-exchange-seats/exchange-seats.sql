0d0cc1d60780a43418b5d7a7f9011f78b2a7a658
SA <saworks26@gmail.com>
2026-06-18 21:11:40 +0530

    Update exchange-seats.sql

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,11 +1,9 @@
 # Write your MySQL query statement below
 SELECT 
-    CASE 
-        
-        WHEN id % 2 = 1 AND id < (SELECT MAX(id) FROM seat) THEN id + 1
-        WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM seat) THEN id
-        ELSE id - 1
-    END AS id, 
-    student
-FROM seat
-ORDER BY id;
+    s1.id, 
+    COALESCE(s2.student, s1.student) AS student
+FROM seat s1
+LEFT JOIN seat s2 
+    ON (s1.id % 2 = 1 AND s1.id + 1 = s2.id)   -- Odd rows look for the next even row
+    OR (s1.id % 2 = 0 AND s1.id - 1 = s2.id)   -- Even rows look for the previous odd row
+ORDER BY s1.id;
