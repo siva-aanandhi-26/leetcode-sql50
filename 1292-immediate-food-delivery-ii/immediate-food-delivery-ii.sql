8c81987240ac990a7f5346ea5f5fff651d932e58
SA <saworks26@gmail.com>
2026-06-21 22:58:28 +0530

    Time: 800 ms (14.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,11 @@
+SELECT 
+    ROUND(
+        SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 
+        2
+    ) AS immediate_percentage
+FROM delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM delivery
+    GROUP BY customer_id
+);
