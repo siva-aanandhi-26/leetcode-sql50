f760bf8c2ae177fa98691ed09f3d9c3faa83c7dd
SA <saworks26@gmail.com>
2026-06-21 15:01:00 +0530

    Time: 733 ms (50.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,9 @@
+SELECT s.product_id, s.year AS first_year, s.quantity, s.price
+FROM sales s
+JOIN (
+    SELECT product_id, MIN(year) AS first_year
+    FROM sales
+    GROUP BY product_id
+) t
+ON s.product_id = t.product_id
+AND s.year = t.first_year;
